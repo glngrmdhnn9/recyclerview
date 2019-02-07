65627020ad91920455d2b1fe76743ae78f41beae
glngrmdhnn <glngrmdhnn@gmail.com>
2019-02-07 21:58:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pljc_pc/recyclerview/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.pljc_pc.recyclerview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+AlertDialog.Builder alertDialog;
+LayoutInflater inflater;
+
+EditText edtnama;
+RadioGroup rGrup;
+RadioButton rbIK,rbSI,rbTI;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                alertDialog = new AlertDialog.Builder(MainActivity.this);
+                inflater = getLayoutInflater();
+                view = inflater.inflate(R.layout.input_layout,null);
+
+                final View dialogView = view;
+
+                alertDialog.setView(dialogView);
+                alertDialog.setCancelable(true);
+                alertDialog.setIcon(R.mipmap.ic_launcher);
+                alertDialog.setTitle("Biodata");
+
+
+
+
+
+
+
+
+
+
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
